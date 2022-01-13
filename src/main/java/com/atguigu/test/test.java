43746d530787449c719c6c8f54fd95254b8022ab
yuuuu <2697161372@qq.com>
2022-01-13 20:33:50 +0800

    first commit

---
+++ src/main/java/com/atguigu/test/test.java
@@ -0,0 +1,11 @@
+package com.atguigu.test;
+
+/**
+ * @author yuuuu
+ * @create 2022-01-13 20:30
+ */
+public class test {
+    public static void main(String[] args) {
+        System.out.println("Hello,Git!");
+    }
+}
