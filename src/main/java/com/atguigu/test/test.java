7a958ce569e79992bdaa38a47f3ec1757fc9fc9a
yuuuu <2697161372@qq.com>
2022-01-13 22:11:01 +0800

    master commit2

--- src/main/java/com/atguigu/test/test.java
+++ src/main/java/com/atguigu/test/test.java
@@ -11,5 +11,6 @@ public class test {
         System.out.println("Hello,Git3!");
         System.out.println("Hello,Git4!");
         System.out.println("Hello,Git5!");
+        System.out.println("Hello,Git6!");
     }
 }
