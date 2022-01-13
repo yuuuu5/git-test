febcce2b459cdf98b2e2255f78ddbf6c5c8203ed
yuuuu <2697161372@qq.com>
2022-01-13 20:37:02 +0800

    second commit

--- src/main/java/com/atguigu/test/test.java
+++ src/main/java/com/atguigu/test/test.java
@@ -7,5 +7,6 @@ package com.atguigu.test;
 public class test {
     public static void main(String[] args) {
         System.out.println("Hello,Git!");
+        System.out.println("Hello,Git2!");
     }
 }
