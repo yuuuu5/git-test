019328cb6a2cf9f3ecf64b9cdaca1e9fceec24de
yuuuu <2697161372@qq.com>
2022-01-13 20:40:00 +0800

    hot-fix commit

--- src/main/java/com/atguigu/test/test.java
+++ src/main/java/com/atguigu/test/test.java
@@ -8,5 +8,6 @@ public class test {
     public static void main(String[] args) {
         System.out.println("Hello,Git!");
         System.out.println("Hello,Git2!");
+        System.out.println("Hello,Git3!");
     }
 }
